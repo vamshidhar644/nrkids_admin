91dc82228444376b6958fbe14311c514f5326fb8
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2023-07-26 22:17:00 +0530

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route
-            path="/"
+            path="/" 
             element={user ? <Dashboard /> : <Navigate to="/login" />}
           />
           <Route
